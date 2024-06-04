12484ea89b8b81b6970fdd55aecca80623a4a3db
JuanPabloGomezLopez <juan.gomez148@udea.edu.co>
2024-06-04 15:40:45 -0500

    Initial commit

---
+++ src/test/java/com/udea/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
