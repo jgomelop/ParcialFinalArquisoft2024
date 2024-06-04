43cffa2bf8c76f12b0307b1bd6b097fa33da38f4
JuanPabloGomezLopez <juan.gomez148@udea.edu.co>
2024-06-04 18:27:56 -0500

    adding methods to Repository interfaces

--- src/main/java/com/udea/demo/domain/repository/IEmployeeRepository.java
+++ src/main/java/com/udea/demo/domain/repository/IEmployeeRepository.java
@@ -3,5 +3,8 @@ package com.udea.demo.domain.repository;
 import com.udea.demo.domain.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IEmployeeRepository extends JpaRepository<Employee, Long> {
+    List<Employee> findAllByProjectsId(Long id);
 }
--- src/main/java/com/udea/demo/domain/repository/IProjectRepository.java
+++ src/main/java/com/udea/demo/domain/repository/IProjectRepository.java
@@ -3,5 +3,8 @@ package com.udea.demo.domain.repository;
 import com.udea.demo.domain.models.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IProjectRepository extends JpaRepository<Project, Long> {
+    List<Project> findAllByEmployeesDocument(String document);
 }
