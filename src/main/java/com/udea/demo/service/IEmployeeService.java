7a46c627c2196898cc71055df69d621c80f9895d
JuanPabloGomezLopez <juan.gomez148@udea.edu.co>
2024-06-04 17:25:39 -0500

    adding methods to Services interfaces

--- src/main/java/com/udea/demo/service/IEmployeeService.java
+++ src/main/java/com/udea/demo/service/IEmployeeService.java
@@ -1,4 +1,7 @@
 package com.udea.demo.service;
 
+import com.udea.demo.domain.models.Employee;
+
 public interface IEmployeeService {
+    Employee createEmployee(Employee employee);
 }
--- src/main/java/com/udea/demo/service/IProjectService.java
+++ src/main/java/com/udea/demo/service/IProjectService.java
@@ -1,4 +1,7 @@
 package com.udea.demo.service;
 
+import com.udea.demo.domain.models.Project;
+
 public interface IProjectService {
+    Project createProject(Project project);
 }
